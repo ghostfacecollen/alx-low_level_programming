ce889b694e316d6403d603a42afd4515249a6522
ghostfacecollen <collenonkemetse@gmail.com>
2023-07-20 18:18:27 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - Test function for positive or negative 
+* main - Test function for positive or negative
 * Return: 0
 */
 
@@ -13,5 +13,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	return (0);
+return (0);
 }
